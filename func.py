46abc9a39909aa86561ecb468fab4b91342a8a37
Raid <113335569+rayths@users.noreply.github.com>
2023-11-29 22:56:04 +0700

    Update func.py

--- func.py
+++ func.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
-def search_jadwal(cursor_jadwal, keyword): # fungsi search
-    query = "SELECT * FROM jadwal_kelas WHERE jadwal = ?"
-    result = cursor_jadwal.execute(query, (keyword,)).fetchall()
+def search_jadwal(cursor_jadwal, keyword, username): # fungsi search
+    query = "SELECT * FROM jadwal_kelas WHERE jadwal = ? AND username_db = ?"
+    result = cursor_jadwal.execute(query, (keyword, username,)).fetchall()
     return result
 
 def display_jadwal(jadwal):
@@ -12,7 +12,7 @@ def display_jadwal(jadwal):
             st.caption(f"{row[4]} - {row[5]}")
             st.text(f"{row[1]} ({row[2]}) | {row[6]}")
             
-def display_search_jadwal(jadwal, jadwal_conn): # fungsi display
+def display_search_jadwal(jadwal, jadwal_conn, username): # fungsi display
     if jadwal:
         for row in jadwal:
             st.markdown(f"### __{row[3]}__")
@@ -21,7 +21,7 @@ def display_search_jadwal(jadwal, jadwal_conn): # fungsi display
     else:
         st.warning("Tidak ada jadwal yang ditemukan, silahkan tambahkan jadwal anda.")
 
-        jadwal_sort = jadwal_conn.execute("SELECT * FROM jadwal_kelas ORDER BY jadwal ASC").fetchall() # inisalisi jadwal sort
+        jadwal_sort = jadwal_conn.execute("SELECT * FROM jadwal_kelas WHERE username_db = ? ORDER BY jadwal ASC", (username,)).fetchall() # inisalisi jadwal sort
 
         st.write("Jadwal Mingguan Anda:")
         display_jadwal(jadwal_sort)
@@ -38,20 +38,18 @@ def update_jadwal(jadwal_conn, cursor_jadwal, new_jadwal):
     jadwal_conn.commit()
     st.success("Jadwal berhasil diubah")
 
-def display_delete_jadwal(jadwal_conn, cursor_jadwal, jadwal):
+def display_delete_update_jadwal(jadwal_conn, cursor_jadwal, jadwal):
     if jadwal:
         for row in jadwal:
             st.markdown(f"### __{row[3]}__")
             st.caption(f"{row[4]} - {row[5]}")
             st.text(f"{row[1]} ({row[2]}) | {row[6]}")
-            col1, col2 = st.columns(2)
+            col1, col2 = st.columns([0.1, 0.9])
             with col1:
                 if st.button("Hapus"):
                     delete_jadwal(jadwal_conn, cursor_jadwal, row[1])
-                    display_delete_jadwal(jadwal_conn, cursor_jadwal, jadwal)
             with col2:
                 if st.button("Ubah"):
                     pass
-                    
     else:
         st.write("Tidak ada jadwal yang ditemukan, silahkan tambahkan jadwal anda.")
