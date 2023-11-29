ca46ea4297c6f56c16d801e75c44d7323c4bcb23
Raid <113335569+rayths@users.noreply.github.com>
2023-11-29 16:34:37 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -7,6 +7,8 @@ import sqlite3 as sql
 import yaml
 from yaml.loader import SafeLoader
 from st_pages import Page, show_pages, add_page_title, hide_pages
+from func import search_jadwal, display_jadwal, display_delete_jadwal, delete_jadwal
+
 
 logo = Image.open("Logo.png")
 st.set_page_config(
@@ -34,7 +36,7 @@ jadwal_ruangkelas_conn = sql.connect("file:jadwal.db?mode=rwc", uri=True)
 # database notes
 notes_conn = sql.connect("file:notes.db?mode=rwc", uri=True)
 #notes_conn.execute("DROP TABLE notes")
-notes_conn.execute("CREATE TABLE IF NOT EXISTS notes (id INTEGER PRIMARY KEY AUTOINCREMENT, judul VARCHAR(255), notes TEXT, tanggal VARCHAR(255))")
+#notes_conn.execute("CREATE TABLE IF NOT EXISTS notes (id INTEGER PRIMARY KEY AUTOINCREMENT, judul VARCHAR(255), notes TEXT, tanggal VARCHAR(255))")
 
 # autentikasi
 authenticator = stauth.Authenticate(
@@ -62,24 +64,52 @@ if authentication_status:
         # side bar customization
         st.sidebar.write('''<div style="text-align:center">
                          <img src="http://drive.google.com/uc?export=view&id=1vsVDyaQ1N3AMoysnDvEjBAeIZwWV9l92" width="125">
-                         </div>''', unsafe_allow_html=True)
-        st.sidebar.text("")
-        st.sidebar.text("")
-        st.sidebar.write(f":blue[**Selamat Datang di Data Smart, {name}!!**]")
+                         <br><br></div>''', unsafe_allow_html=True)
+        st.sidebar.write(f":blue[**Selamat Datang di Data Smart, <br> {name} !!**]", unsafe_allow_html=True)
 
         st.header(":blue[Data Smart]", divider="orange")
-        selected_tab = option_menu(menu_title=None, options=["Home", "Data Remind", "Data Search", "Data Notes"], icons=["house", "bell", "search", "bookmarks"], \
+        selected_tab = option_menu(menu_title=None, options=["Home", "Data Remind", "Data Search", "Data Notes"], icons=["house", "bell", "search", "bookmarks"], 
                                     key="nav", orientation="horizontal",)
 
         if selected_tab == "Home":
             st.header(":blue[Welcome to Data Smart!]", divider="grey")
-            st.write("Aktivitas yang padat sering kali membuat seseorang sulit untuk mengingat beberapa hal yang ada dalam rutinitas sehari-harinya. \
-                      Apalagi sebagai seorang mahasiswa hal tersebut merupakan hal yang sering terjadi, misalnya dalam hal jadwal harian, ruangan kelas, maupun tenggat tugas yang mereka miliki. \
-                     Dengan adanya aplikasi data smart dapat membantu pengguna mengelola waktu dan tanggung jawab dengan memberikan notifikasi yang dapat disesuaikan.")
+            st.write('''<div style="text-align: justify">
+                     <h3>Apa Itu Data Smart?</h3>
+                     Data smart merupakan aplikasi di bidang pendidikan yang berbasis teknologi.
+                     Sederhananya pengertian Data Smart merupakan kerangka management untuk memudahkan pengguna dalam mengelola waktu dan tanggung jawab mereka.
+                     </div>''', unsafe_allow_html=True)
+            
+            st.write("<hr>", unsafe_allow_html=True)
+            st.write('''<div style="text-align: justify">
+                     <h3>Latar Belakang</h3>
+                     Aktivitas yang padat sering kali membuat seseorang sulit untuk mengingat beberapa hal yang ada dalam rutinitas sehari-harinya. 
+                     Apalagi sebagai seorang mahasiswa hal tersebut merupakan hal yang sering terjadi, misalnya dalam hal jadwal harian, ruangan kelas, maupun tenggat tugas yang mereka miliki. 
+                     Dengan adanya aplikasi data smart dapat membantu pengguna mengelola waktu dan tanggung jawab dengan memberikan notifikasi yang dapat disesuaikan.
+                     </div>''', unsafe_allow_html=True)
+            
+            st.write("<hr>", unsafe_allow_html=True)
+            st.write('''<div style="text-align: justify">
+                     <h3>Data Remind</h3>
+                     Fitur ini membantu pengguna untuk mengingat aktivitas yang seharusnya dilakukan, seperti untuk mengingat tugas yang harus dikerjakan, 
+                     berserta batas waktu pengumpulannya.
+                     </div>''', unsafe_allow_html=True)
+            
+            st.write("<hr>", unsafe_allow_html=True)
+            st.write('''<div style="text-align: justify">
+                     <h3>Data Search</h3>
+                     Fitur ini membantu pengguna untuk menemukan informasi yang diinginkan, seperti ruangan kelas, dan jadwal harian.
+                     </div>''', unsafe_allow_html=True)
+            
+            st.write("<hr>", unsafe_allow_html=True)
+            st.write('''<div style="text-align: justify">
+                     <h3>Data Notes</h3>
+                     Fitur ini membantu pengguna untuk membuat catatan perkuliahan serta mengupload file yang mereka inginkan.
+                     </div>''', unsafe_allow_html=True)
+
 
         elif selected_tab == "Data Remind":
             st.header(":blue[Data Remind]", divider="grey")
-            fitur = st.selectbox("Pilih Menu:", options=["Tugas", "Tambah Tugas"])
+            fitur = st.selectbox("Pilih Menu:", ["Tugas", "Tambah Tugas"])
             if fitur == "Tugas":
                 st.subheader("Tugas")
 
@@ -93,9 +123,12 @@ if authentication_status:
                         st.caption(f"{tanggal_pengumpulan} | {waktu_pengumpulan}")
                         with st.expander("Keterangan: "):
                             st.write(f"{keterangan}")
+
                         if st.button("Done"):
                             with tugas_conn:
                                 tugas_conn.execute("DELETE FROM tugas WHERE nama_tugas = ?", (nama_tugas,))
+                            st.success("Selamat Tugas Anda Sudah Selesai, Silahkan Kerjakan Tugas Lainnyaa!")
+            
                         #if datetime.now() > datetugas:
                         #    st.error("Waktu Pengumpulan Sudah Melewati Tenggat")
                 else:
@@ -128,41 +161,42 @@ if authentication_status:
             if fitur == "Jadwal dan Ruang Kelas":
                 st.subheader("Jadwal dan Ruang Kelas")
 
-                def search_data(keyword):
-                    cursor_jadwal = jadwal_ruangkelas_conn.cursor()
-                    cursor_jadwal.execute("SELECT * FROM jadwal_kelas WHERE mata_kuliah = ?", (keyword,))
-
-                    results = cursor_jadwal.fetchall()
-
-                    return results
-
-                keyword = st.text_input("Masukkan mata kuliah pencarian:")
+                menu_datasearch = st.selectbox("Pilih Menu:",["Cari Jadwal", "Hapus & Ubah Jadwal"])
                 
-                if st.button("Cari"):
-                    # Memanggil fungsi pencarian
-                    search_result = search_data(keyword)
+                if menu_datasearch == "Cari Jadwal":
 
-                    # Menampilkan hasil pencarian
-                    if not search_result:
-                        st.warning("Tidak ada hasil yang ditemukan.")
-                    else:
+                    cursor_jadwal = jadwal_ruangkelas_conn.cursor()
+                    keyword = st.text_input("Masukkan hari:")
+
+                    cari = st.button("Cari")
+                    if cari == True:
+                        # Memanggil fungsi pencarian
+                        search_result = search_jadwal(cursor_jadwal, keyword)
+
+                        # Menampilkan hasil pencarian
                         st.write("Hasil Pencarian:")
-                        st.write(search_result)
+                        display_jadwal(search_result)
 
-                jadwal = jadwal_ruangkelas_conn.execute("SELECT mata_kuliah, kelas, jadwal, jam_mulai, jam_akhir, ruang_kelas FROM jadwal_kelas").fetchall()
+            
+                    elif cari == False:
 
-                if jadwal:
-                    for row in jadwal:
-                        mata_kuliah, kelas, jadwal, jam_mulai, jam_akhir, ruang_kelas = row
-                        st.markdown(f"### __{jadwal}__")
-                        st.caption(f"{jam_mulai} - {jam_akhir}")
-                        st.text(f"{mata_kuliah} ({kelas}) | {ruang_kelas}")   
+                        jadwal_sort = jadwal_ruangkelas_conn.execute("SELECT * FROM jadwal_kelas ORDER BY jadwal ASC").fetchall()
 
+                        st.write("<h4>Jadwal Mingguan Anda:</h4>", unsafe_allow_html = True)
+                        display_jadwal(jadwal_sort)
+
+                elif menu_datasearch == "Hapus & Ubah Jadwal":
+
+                    jadwal_sort = jadwal_ruangkelas_conn.execute("SELECT * FROM jadwal_kelas ORDER BY jadwal ASC").fetchall()
+                    cursor_jadwal = jadwal_ruangkelas_conn.cursor()
+
+                    st.write("<h4>Pilih Jadwal Untuk Di Hapus:</h4>", unsafe_allow_html = True)
+                    display_delete_jadwal(jadwal_ruangkelas_conn, cursor_jadwal, jadwal_sort)
 
             elif fitur == "Tambah Jadwal dan Ruang Kelas":
                 st.subheader("Tambah Data")
                 matkul = st.text_input("Mata Kuliah:")
-                hari = str(st.selectbox("Pilih Hari:", ["Senin","Selasa","Rabu","Kamis","Jumat","Sabtu","Minggu"]))
+                hari = str(st.selectbox("Pilih Hari:", ["Senin","Selasa","Rabu","Kamis","Jumat","Sabtu"]))
                 jam_mulai = str(st.time_input("Waktu Mulai:"))
                 jam_akhir = str(st.time_input("Waktu Selesai:"))
                 kelas = st.text_input("Kelas:")
@@ -188,13 +222,15 @@ if authentication_status:
                         judul, notes, tanggal = row
                         st.markdown(f"#### __{judul}__")
                         st.caption(f"{tanggal}")
-                        st.text(f"{notes}") 
+                        st.write(f'''<div style="text-align: justify">
+                                 {notes}
+                                 </div>''', unsafe_allow_html=True) 
 
             elif fitur == "Tambah Notes":
                 st.subheader("Tambah Notes")
                 judul = st.text_input("Judul:")
                 tanggal_notes = st.date_input("Tanggal:")
-                notes = st.text_area("Masukkan teks anda disini ...")
+                notes = st.text_area("Masukkan teks anda disini:")
 
                 if st.button("Tambah"):
                     with notes_conn:
